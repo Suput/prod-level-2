4d0d9c7973bfb8c9bb182bdbfd2d26777e5b96e4
Nick <nikitamm59rus@gmail.com>
2021-12-13 17:37:13 +0300

    Add comp, add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormTestComponent } from './form-test/form-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormTestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
